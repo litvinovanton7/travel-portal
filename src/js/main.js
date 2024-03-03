8bd801fc136a6f7a67fd26d9bf058da22a6bd23f
Anton <antonafonin154@gmail.com>
2024-03-03 18:17:59 -0500

    started hamburger menu

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const habmurger = document.querySelector(".hamburger__menu");
+    const headerMenu = document.querySelector(".header__menu");
+
+    habmurger.addEventListener("click", mobileMenu);
+
+    function mobileMenu() {
+        habmurger.classList.toggle("active");
+        headerMenu.classList.toggle("active");
+    }
+});
